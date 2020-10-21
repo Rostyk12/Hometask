1f4379325351f47537781f9b3b2680d36f9513e8
Rostyk <ros123ole@gmail.com>
2020-10-21 16:08:23 +0300

    перший коміт

---
+++ .idea/scr/com/ros/g/program1.java
@@ -0,0 +1,29 @@
+package com.ros.g;
+import java.util.Scanner;
+public class program1 {
+    public static void main(String[] args) {
+        Scanner srt = new Scanner(System.in);
+        int a,b,c;
+        System.out.println("a = ");
+        a = srt.nextInt();
+        System.out.println("b = ");
+        b = srt.nextInt();
+        System.out.println("c = ");
+        c = srt.nextInt();
+        double x1,x2,d;
+
+        d = Math.pow(b,2) - 4*a*c;
+        System.out.println(d);
+        if (d>=0){
+            x1 = (-b + Math.pow(d,0.5)) / (2*a);
+            x2 = (-b - Math.pow(d,0.5)) / (2*a);
+            System.out.println("Результат: x1 = " + x1 + "x2  = " + x2);
+
+        }
+        else {
+            System.out.println("Розв'язку немає");
+
+        }
+
+    }
+}

e8522f1c6ee5146fcbc06cf5529e4cb075f1b6e3
Rostyk12 <ros123ole@gmail.com>
2020-10-21 18:17:29 +0300

    2 програма

--- .idea/scr/com/ros/g/program1.java
+++ .idea/scr/com/ros/g/program1.java
@@ -2,29 +2,18 @@ package com.ros.g;
 import java.util.Scanner;
 public class program1 {
     public static void main(String[] args) {
-        Scanner srt = new Scanner(System.in);
-        int a,b,c;
-        System.out.println("a = ");
-        a = srt.nextInt();
-        System.out.println("b = ");
-        b = srt.nextInt();
-        System.out.println("c = ");
-        c = srt.nextInt();
-        double x1,x2,d;
 
-        d = Math.pow(b,2) - 4*a*c;
-        System.out.println(d);
-        if (d>=0){
-            x1 = (-b + Math.pow(d,0.5)) / (2*a);
-            x2 = (-b - Math.pow(d,0.5)) / (2*a);
-            System.out.println("Результат: x1 = " + x1 + "x2  = " + x2);
+        int n = -1;
+        int[] aray = {n};
+        int i;
 
+
+        for (i = 0; i < 55; i++) {
+            n += 2;
+            System.out.println("елемент масиву = " + n);
         }
-        else {
-            System.out.println("Розв'язку немає");
-            System.out.print("123");
 
-        }
 
     }
+
 }
